901f95bebd89fd129f5993a002fdccd8620ac1f7
Matias Sencion <matiassencion@gmail.com>
2024-06-04 23:16:41 -0300

    push

--- OpenGL-basico/raster/color.h
+++ OpenGL-basico/raster/color.h
@@ -15,8 +15,20 @@ public:
     }
     
     color(float red, float green, float blue, float alpha = 255)
-        : red_(red), green_(green), blue_(blue), alpha_(alpha)
     {
+        red_ = red;
+        green_ = green;
+        blue_ = blue;
+        alpha_ = alpha;
+        if ( red < 0 ) red_ = 0;
+        if ( red > 255 ) red_ = 255;
+        if ( green < 0 ) green_ = 0;
+        if ( green > 255 ) green_ = 255;
+        if ( blue < 0 ) blue_ = 0;
+        if ( blue > 255 ) blue_ = 255;
+        if ( alpha < 0 ) alpha_ = 0;
+        if ( alpha > 255 ) alpha_ = 255;
+        
     }
 
     RGBQUAD to_rgb() const;
@@ -29,4 +41,5 @@ public:
     float get_green() const { return green_; }
     float get_blue() const { return blue_; }
 
+    color operator+(const color &c) const;
 };
