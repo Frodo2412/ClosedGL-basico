bdc1b39c3cb05b712d3e8034d868445dbb34cdce
Bruno <brunolemus553@gmail.com>
2024-06-08 19:16:53 -0300

    feat: add `translucency_` and `refractive_index_` to `object` class

--- OpenGL-basico/ray-tracing/object.h
+++ OpenGL-basico/ray-tracing/object.h
@@ -7,18 +7,18 @@ class object
 {
     vector3 position_;
     color color_;
-    double reflectivity_;
-    double shininess_;
-    
+    double reflectivity_, shininess_, translucency_, refractive_index_;
+
 public:
-    object(vector3 pos, color color, double reflectivity, double shininess): position_(pos), color_(color), reflectivity_(reflectivity), shininess_(shininess)
+    object(vector3 pos, color color, double reflectivity, double shininess): position_(pos), color_(color),
+                                                                             reflectivity_(reflectivity),
+                                                                             shininess_(shininess)
     {
     };
-    virtual bool test_intersection(ray &rayo, vector3 &point, vector3 &normal);
+    virtual bool test_intersection(ray& rayo, vector3& point, vector3& normal);
     color get_color();
     vector3 get_position();
     double get_shininess();
     double get_reflectivity();
     virtual ~object();
 };
-
