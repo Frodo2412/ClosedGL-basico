577938e2dda625e7e7fcdeeb1ee048a92acc91f4
Bruno Lemus <78759358+Frodo2412@users.noreply.github.com>
2024-06-15 17:05:47 -0300

    reflexion (#41)

--- OpenGL-basico/ray-tracing/light.cpp
+++ OpenGL-basico/ray-tracing/light.cpp
@@ -2,36 +2,35 @@
 
 #include <iostream>
 
-bool light::compute_illumination(vector3 &intersection_point, vector3 &normal, std::vector<object*> objects, object* current, double & intensity)
+bool light::compute_illumination(vector3& intersection_point, vector3& normal, std::vector<object*> objects,
+                                 object* current, double& intensity)
 {
     intensity = 0.0;
     vector3 rayo_s = (position_ - intersection_point).normalize();
     double prod = normal.dot_product(rayo_s);
-    if(prod >= 0.0)
+    if (prod >= 0.0)
     {
         intensity = get_intensity() * prod;
         /* rayo que va desde el punto de interseccion hacia la luz, si interseca con un objeto significa
           que la fuente de luz esta siendo tapada y por lo tanto tenemos sombra */
         ray sombra = ray(intersection_point, rayo_s);
-        
-        for(object* obj : objects)
+
+        for (object* obj : objects)
         {
             if (obj != current)
             {
                 vector3 inter = {0, 0, 0};
                 vector3 trash1 = {0, 0, 0};
-                if(obj->test_intersection(sombra, inter, trash1))
+                if (obj->test_intersection(sombra, inter, trash1))
                 {
                     double intersection_distance = (inter - intersection_point).get_norm();
                     double light_distance = (position_ - intersection_point).get_norm();
-                    if(intersection_distance > light_distance)//si intersecto con otro objeto pero mas lejos que la ubicacion de la luz, entonces le llega luz
+                    if (intersection_distance > light_distance)
+                    //si intersecto con otro objeto pero mas lejos que la ubicacion de la luz, entonces le llega luz
                     {
                         continue;
                     }
-                    if(obj->get_translucency() != 1.0)//Si el objeto que genera sombra es completamente transparente entonces no genera sombra
-                    {
-                        intensity = 0.0;
-                    }
+                    intensity = std::max(0.0, intensity - 1 + obj->get_translucency());
                     break;
                 }
             }
