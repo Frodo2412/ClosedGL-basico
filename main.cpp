1676c1398821a74a87b2b38c04c3d0e2654b51a0
Bruno <brunolemus553@gmail.com>
2024-06-15 16:31:17 -0300

    fix cambios

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ int main(int argc, char* argv[])
             renderer::render_image(img, renderer); // renders image to screen
         }
         std::cout << "Scene rendered successfully.\n";
-        
+
         SDL_Event e;
         bool quit = false;
         while (!quit)
@@ -74,7 +74,7 @@ int main(int argc, char* argv[])
                 }
             }
         }
-        
+
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
