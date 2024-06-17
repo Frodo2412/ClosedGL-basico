114a699d1fc2bf54181f9d09bd480f3eac2ab96f
sofiu <156955245+sofiieww@users.noreply.github.com>
2024-06-17 18:08:36 -0300

    creo q bien (#49)

--- OpenGL-basico/graphics/renderer.h
+++ OpenGL-basico/graphics/renderer.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "image.h"
+#include "SDL.h"
 
 class renderer
 {
 public:
+    static int loaded_;
     static void render_image(image& image);
+    static void render_intermedium_image(image& img, int max_x, SDL_Renderer* renderer);
 };
